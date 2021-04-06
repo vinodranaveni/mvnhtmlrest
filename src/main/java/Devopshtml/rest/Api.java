4a878f022984a5b316d13a699094c7b01781711d
Vinod Ranaveni <ranaveni.vinod54@gmail.com>
2021-04-07 04:53:56 +0530

    html rest mvn template

---
+++ src/main/java/Devopshtml/rest/Api.java
@@ -0,0 +1,11 @@
+package Devopshtml.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author SERPRO
+ */
+@ApplicationPath("api")
+public class Api extends Application {
+}
